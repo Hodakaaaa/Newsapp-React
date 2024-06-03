cbf2f051689f357cce6a197b43d713f658c57b4a
Mandhoj Thing <np03cs4s220391@heraldcollege.edu.np>
2024-06-03 09:26:53 +0545

    Day27:Fetch Api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,10 +7,10 @@ export default class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+        <img src={!imageUrl?"https://ambcrypto.com/wp-content/uploads/2024/06/eth-1536x878.png":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
